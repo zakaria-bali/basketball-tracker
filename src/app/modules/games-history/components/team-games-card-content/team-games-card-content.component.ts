d278f9230ef9e897b1a67f9668362f42a87237cf
zakaria-bali <zakaria99bali@gmail.com>
2023-02-16 01:07:08 +0100

    feat(team-games-card): reaplace team games card by base card and team games card content

---
+++ src/app/modules/games-history/components/team-games-card-content/team-games-card-content.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { TeamDetails } from 'src/app/core/models/nba-data.model';
+
+@Component({
+  selector: 'app-team-games-card-content',
+  templateUrl: './team-games-card-content.component.html',
+  styleUrls: ['./team-games-card-content.component.scss']
+})
+export class TeamGamesCardContentComponent implements OnInit {
+  @Input()
+  team: TeamDetails | undefined;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/modules/games-history/games-history.module.ts
+++ src/app/modules/games-history/games-history.module.ts
@@ -3,14 +3,14 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { GamesHistoryRoutingModule } from './games-history-routing.module';
 import { GamesHistoryShellComponent } from './games-history-shell.component';
 import { GameScoreComponent } from './components/game-score/game-score.component';
-import { TeamGamesCardComponent } from './components/team-games-card/team-games-card.component';
+import { TeamGamesCardContentComponent } from './components/team-games-card-content/team-games-card-content.component';
 
 
 @NgModule({
   declarations: [
     GamesHistoryShellComponent,
     GameScoreComponent,
-    TeamGamesCardComponent
+    TeamGamesCardContentComponent
   ],
   imports: [
     GamesHistoryRoutingModule,
